1ffd9ffde77f328d05a1e7127679cb287cbf12d1
MsFire <learntv7@gmail.com>
2018-11-10 19:04:03 +0100

    Update index.js

--- Slicy/starts/index.js
+++ Slicy/starts/index.js
@@ -14,17 +14,19 @@ window.addEventListener('mousemove', function(ev) {
     mouse[1] = ev.clientY;
 });
 
+let access = true;
+
 window.onload = function() {
 
     context = document.getElementById('canvas').getContext('2d');
     player = new Player();
 
     setInterval(() => {
-
+        if(access) {
         draw(context);
-        update();
+        update(context);
+        }
     }, Interval);
-
     // setInterval(() => {
     //     player.addMass(0.2);
     // }, player.mass / Interval);
@@ -35,7 +37,7 @@ window.onload = function() {
 
 };
 
-function update() {
+function update(c) {
     player.update();
     var RandomTurbo = Math.max(5, Math.floor((Math.random() * 10)));
     if(eaten === RandomTurbo && turboStarted) {
@@ -51,19 +53,51 @@ function update() {
         if(collides(player, enemies[i]) && player.mass > enemies[i].mass) {
             player.addMass(enemies[i].mass*0.1111);
             enemies.splice(i, 1);
+            player.addKill();
             eaten++;
             turboStarted = true;
         } else if(collides(player, enemies[i]) && player.mass < enemies[i].mass) {
             console.log('end');
+            access = false;      
+
+            var timeSurvived = (Date.now() - player.timeStarted);
+            clearCanvas(c);
+            foods.forEach((food) => {
+                if(food.x < W-200) { food.draw(c) }
+                if(food.y < H-400) { food.draw(c)}
+            });
+            c.beginPath();
+            c.fillStyle = `rgb(${enemies[i].r}, ${enemies[i].g}, ${enemies[i].b})`;
+            c.rect(50, 50, Math.min(enemies[i].mass,enemies[i].mass, 200), Math.min(enemies[i].mass,enemies[i].mass, 200));
+            c.fill();
+            c.closePath();
+            c.font = "15px Arial";
+            c.strokeText(`Killer`, 50, 70+enemies[i].mass);
+            c.fillStyle = "#4f5359";
+            c.beginPath();
+            c.rect(W/2-315, H/2-140, W/2-100, H/2-20);
+            c.fill();
+            c.strokeStyle = "black";
+            c.stroke();
+            c.closePath();
+            c.font = "30px Arial";
+            c.strokeText(`Final Mass: ${Math.round(player.mass)}`, W/2-150, H/2);
+            c.strokeText(`Kills: ${Math.round(player.kills)}`, W/2-150, H/2+50);
+            c.strokeText(`Food Eaten: ${Math.round(player.foodEaten)}`, W/2-150, H/2+100);
+            c.strokeText(`Time Survived: ${new Date(timeSurvived).getMinutes() > 0 ? `${new Date(timeSurvived).getMinutes()}min:${new Date(timeSurvived).getSeconds()}sec`:`${new Date(timeSurvived).getSeconds()}sec`}`, W/2-150, H/2+150);
             enemies = [];
-            foods = [];
-            player = new Player();
-            eaten = 0;
-            turboStarted = false;
+            canvas.onclick = function() {
+                foods = [];
+                player = new Player();
+                eaten = 0;
+                turboStarted = false;
+                access = true;
+            }
         }
     }
     for(var f = 0; f < foods.length; f++) {
         if(collides(player, foods[f]) && player.mass > foods[f].mass) {
+            player.addFoodEat();
             player.addMass(foods[f].mass*0.1);
             foods.splice(f, 1);
         }
